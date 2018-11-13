dfe34b36b89f46de746186f2fa003c007c52a43d
nh0ckjapan <31547509+nh0ckjapan@users.noreply.github.com>
2018-11-14 00:20:08 +0700

    Add project files.

---
+++ MyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyWeb.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<Models.CustomizeUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ MyWeb/Models/CustomizeUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWeb.Models
+{
+    public class CustomizeUser : IdentityUser
+    {
+        [PersonalData]
+        public string FullName { get; set; }
+    }
+}
